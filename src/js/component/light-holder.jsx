2ac00dee15f82010a2f0ef4204ee45adf1018e12
Raul Andrial <rauljandrial@gmail.com>
2022-10-27 00:23:25 +0000

    Added onClick event

--- src/js/component/light-holder.jsx
+++ src/js/component/light-holder.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Light from './light.jsx';
-import SLight from './slight.jsx';
+// import SLight from './slight.jsx';
 
 const LightHolder = () => {
 
@@ -9,12 +9,11 @@ const LightHolder = () => {
         backgroundColor: "black",
         width: "8rem",
         boxSizing: "content-box",
-        // boxShadow: "0 0 30rem 3rem yellow"
     };
 
     return (
-        <div style={styles} className="border border-dark border-5 p-2 mx-auto">
-			<SLight color="red"/>
+        <div style={styles} className="border border-dark border-5 rounded p-2 mx-auto">
+			<Light color="red"/>
 			<Light color="#ff9933" />
 			<Light color="green"/>
 		</div>
--- src/js/component/light.jsx
+++ src/js/component/light.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Light = ({color}) => {
+
+    const [state, setState] = useState({color: color, selected: false});
 
     const styles = {
         minWidth: "8rem",
         minHeight: "8rem",
-        backgroundColor: color,
+        backgroundColor: state.color,
         borderRadius: "50%",
         filter: "brightness(80%)"
     };
 
     return (
-        <div style={styles} className="my-2"></div>
+        <div 
+            style={state.selected ? {...styles, boxShadow: `0 0 6rem 3rem ${state.color}`} : styles}
+            className="my-2"
+            onClick={() => setState({...state, selected: !state.selected})}
+        >
+        </div>
     );
 };
 
