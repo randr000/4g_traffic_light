ee3f21fe34b81bbef48fe9df880a314566331cac
Raul Andrial <rauljandrial@gmail.com>
2022-10-28 22:00:17 +0000

    Added onClick event to purple button

--- src/js/component/purple-button.jsx
+++ src/js/component/purple-button.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
-const PurpleButton = () => {
+const PurpleButton = ({purple, setPurple}) => {
 
     return (
-        <button className="p-2 fw-bold fs-1 btn mt-5 text-white" style={{backgroundColor: 'purple'}} onClick={() => setCycle(false)}>
-            <p>Remove</p>
+        <button 
+            className="p-2 fw-bold fs-1 btn mt-5 text-white" 
+            style={{backgroundColor: 'purple'}}
+            onClick={() => setPurple(!purple)}
+        >
+            <p>{purple ? 'Remove' : 'Add'}</p>
             <p>Purple</p>
         </button>
     );
